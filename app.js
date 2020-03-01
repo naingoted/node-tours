7462bafe932b5070fa25e2280b8b79849e41d10a
Vincent Oo <naingoted@gmail.com>
2020-03-01 17:42:50 +0800

    set up api for tours-simple data

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const morgan = require('morgan');
+
+const userRouter = require('./routes/userRoutes');
+const tourRouter = require('./routes/tourRoutes');
+const app = express();
+
+// Middlewars
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
+app.use((req, res, next) => {
+  console.log(' Hello from middleware ');
+  next();
+});
+
+app.use((req, res, next) => {
+    req.requestTime = new Date().toISOString();
+    next();
+})
+
+// Routes
+app.use('/api/v1/users', userRouter);
+app.use('/api/v1/tours', tourRouter);
+
+module.exports = app;
